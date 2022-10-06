c123f94a31cc56497d31a1f0842b8c49619b107a
L_Beno <benoit@ahgency.be>
2022-10-06 14:27:35 +0200

    test form front

--- api/inscription/index.js
+++ api/inscription/index.js
@@ -21,7 +21,7 @@ module.exports = async function (context,req,res) {
                 }
                 else {
                     console.log("connection established.");
-                    insertUser();
+                    insertUser(req);
                 }
             }
         )
--- frontend/src/Component/Inscription/Inscription.js
+++ frontend/src/Component/Inscription/Inscription.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {useForm} from 'react-hook-form';
 
 import './Inscription.css';
 
@@ -10,16 +11,21 @@ import Col from 'react-bootstrap/Col';
 
 export default function Inscription() {
 
-    const handleSubmit = (e) => {
+    const {handleSubmit} = useForm();
+    const onSubmit = (data, e) => {
+
+        console.log('ha!')
         e.preventDefault(); //on empêche le refresh de la page, nécessaire pour garder les infos déjà présente lors d'un submit érronés
         
         let inscForm = document.getElementById('inscForm'); //on récupère l'élement <form> et ces différents <input>
         let myInscr = new FormData(inscForm); //que l'on intègre à un formData
 
+        console.log(myInscr);
+
         fetch(`/api/inscription`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({body: myInscr})
+            body: JSON.stringify(data)
         })
         .then(response => {
             e.target.reset();
@@ -30,15 +36,13 @@ export default function Inscription() {
 
     }
 
-
-
   return (
     <div id="page_inscription">
         <Container>
             <Row className="inscription_form">
                 <Col lg={12}>
                     <h1>CREER MON COMPTE</h1>
-                <form id="inscForm" onSubmit={handleSubmit}>
+                <form id="inscForm" onSubmit={handleSubmit(onSubmit)}>
                     <label>Nom<br/>
                         <input type="text" name="nom" placeholder="Doe" required/>
                     </label><br/>
