103200b593567418b06ca5d5416c5d3ae292661c
L_Beno <benoit@ahgency.be>
2022-10-10 13:11:35 +0200

    reponse ok

--- api/form/index.js
+++ api/form/index.js
@@ -2,8 +2,11 @@ const mysql = require('mysql');
 const fs = require('fs');
 const {con} = require('../db/db.js');
 const bcrypt = require("bcrypt");
+const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
     try {
       con.connect(
         function (err){
@@ -12,75 +15,72 @@ module.exports = async function (context, req) {
             throw err;
           }
           else {
+
             console.log("connection established.");
-            form();
+            let mail = context.req.body.email;
+            let pwd = context.req.body.pwd;
+          
+            let reqCheckID = "SELECT ID FROM users WHERE Email = '" + mail + "'";
+            let reqPwd = "SELECT Password FROM users WHERE Email = '" + mail + "'";
+
+            con.query(reqCheckID, function (err, result) {
+              if (err) throw err;
+
+              if (result.length === 1){
+                  con.query(reqPwd, function (err, resultat){
+
+                      if (err) throw err;
+                      let passRes = resultat[0].Password;
+                      let sql = "SELECT * FROM users WHERE Email = '" + mail + "' and Password = '" + passRes + "'";
+
+                      bcrypt.compare(pwd, passRes, (err, resp) => {
+                          if (err) throw err;
+                          if (!resp) {
+                              console.log('nul');
+                          }
+                          else {
+                              con.query(sql,function (err, rlt) {
+                                  if (err) throw err;
+                                  const lareponse = rlt
+                                        ? "Reponse : " + JSON.stringify(rlt[0]) 
+                                        : "Erreur dans la reception des données";
+
+                                  console.log('Vous êtes connecté'); 
+                                  console.log(lareponse);
+
+                                  context.res = {
+                                    Headers: {"Content-Type": "application/json"},
+                                    body: lareponse
+                                }
+                              });
+                          }
+                      })
+                  })
+              }
+              else {
+                  context.res = {
+                    status: 401,
+                    body : 'mauvais mail'
+                  };
+              }
+            })
           }
         }
-      )
 
-      function form(){
-        let mail = context.req.body.email;
-        let pwd = context.req.body.pwd;
-        
-        
-        let reqCheckID = "SELECT ID FROM users WHERE Email = '" + mail + "'";
-        let reqPwd = "SELECT Password FROM users WHERE Email = '" + mail + "'";
+    )
 
-        con.query(reqCheckID, function (err, result) {
-            if (err) throw err;
-            console.log('AAA');
+  context.res = {
+    body : 'Yeah'
+  }
 
-            if (result.length === 1){
-                con.query(reqPwd, function (err, resultat){
-
-                    console.log('BBBB');
-                    if (err) throw err;
-                    let passRes = resultat[0].Password;
-
-                    let sql = "SELECT * FROM users WHERE Email = '" + mail + "' and Password = '" + passRes + "'";
-
-                    bcrypt.compare(pwd, passRes, (err, resp) => {
-                        if (err) throw err;
-                        if (!resp) {
-                            console.log('nul');
-                            context.res = {
-                              status :401,
-                              body: 'mauvais mot de passe' 
-                            };
-                        }
-                        else {
-                            //let TKEN = jwt.sign({userID: result[0].ID},'JETETIENSTUMETIENSPARLABARBICHETTELEPREMIERDENOUSDEUXQUIRRIAAURAUNETAPETTE', { expiresIn: '24h'});
-                            con.query(sql,function (err, rlt) {
-
-                              console.log('HIIII');
-                              console.log(rlt);
-                                if (err) throw err;
-                                //rlt.push({token: TKEN})
-                                console.log(rlt);
-                                console.log('Vous êtes connecté');
-                                context.res = {
-                                  status: 200,
-                                  headers:{ "Content-Type": "application/json" },
-                                  body : JSON.parse(rlt[0].ID)
-                            }});
-                        }
-                    })
-                })
-            }
-            else {
-                context.res = {
-                  status: 401,
-                  body : 'mauvais mail'
-                };
-            }
-        })
-      }
-
-    } catch(error) {
-      const err = JSON.stringify(error);
+  }
+      
+      catch(error) {
+      const err = error;
       context.res = {
         status: 500,
         body: `Request error. ${err}`
       };
-    }
-  };
+    } 
+        
+};
