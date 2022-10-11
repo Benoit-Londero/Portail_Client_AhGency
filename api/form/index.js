28e19bbc4adedda65e25a49291f5cf98d7b28a86
L_Beno <benoit@ahgency.be>
2022-10-11 10:29:34 +0200

    GItPushTaMere

--- api/form/index.js
+++ api/form/index.js
@@ -5,84 +5,79 @@ const bcrypt = require("bcrypt");
 const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log('JavaScript HTTP trigger function processed a request.');
 
-    try {
-      con.connect(
-        function (err){
-          if(err){
-            console.log("!!! Cannot connect !!! Error:")
-            throw err;
-          }
-          else {
+  try {
+    con.connect(function (err){
+      if(err){
+        console.log("!!! Cannot connect !!! Error:")
+        throw err;
+      }
+      else {
+        console.log("connection established.");
+        let lareponse = {};
+        comparePass();
+      }
+    })
 
-            console.log("connection established.");
-            let mail = context.req.body.email;
-            let pwd = context.req.body.pwd;
-          
-            let reqCheckID = "SELECT ID FROM users WHERE Email = '" + mail + "'";
-            let reqPwd = "SELECT Password FROM users WHERE Email = '" + mail + "'";
+    function comparePass(){
+      
 
-            con.query(reqCheckID, function (err, result) {
-              if (err) throw err;
+      let mail = context.req.body.email;
+      let pwd = context.req.body.pwd;
+      
+      let reqCheckID = "SELECT ID FROM users WHERE Email = '" + mail + "'";
+      let reqPwd = "SELECT Password FROM users WHERE Email = '" + mail + "'";
 
-              if (result.length === 1){
-                  con.query(reqPwd, function (err, resultat){
+      con.query(reqCheckID, function (err, result) {
+        if (err) throw err;
 
-                      if (err) throw err;
-                      let passRes = resultat[0].Password;
-                      let sql = "SELECT * FROM users WHERE Email = '" + mail + "' and Password = '" + passRes + "'";
+        if (result.length === 1){
+          con.query(reqPwd, function (err, resultat){
 
-                      bcrypt.compare(pwd, passRes, (err, resp) => {
-                          if (err) throw err;
-                          if (!resp) {
-                              console.log('nul');
-                              context.res = {
-                                status :401,
-                                body: 'mauvais mot de passe' 
-                              };
-                          }
-                          else {
-                              con.query(sql,function (err, rlt) {
-                                  if (err) throw err;
-                                  const lareponse = rlt
-                                        ? "Reponse : " + JSON.stringify(rlt[0]) 
-                                        : "Erreur dans la reception des données";
+          if (err) throw err;
+          let passRes = resultat[0].Password;
+          let sql = "SELECT * FROM users WHERE Email = '" + mail + "' and Password = '" + passRes + "'";
 
-                                  console.log('Vous êtes connecté'); 
-                                  console.log(lareponse);
+          bcrypt.compare(pwd, passRes, (err, resp) => {
+            if (err) throw err;
+            if (!resp) {
+              console.log('nul');
+              return 'mauvais mot de passe';
+            }
+            else {
+              con.query(sql,function (err, rlt) {
+                if (err) throw err;
+                lareponse = rlt
+                  ? "Reponse : " + JSON.stringify(rlt[0])
+                  : "Erreur dans la reception des données";
+                  
+                  console.log('Vous êtes connecté');
+                  console.log(lareponse);
 
-                                  
-                              });
-                          }
-                      })
-                  })
-              }
-              else {
-                  context.res = {
-                    status: 401,
-                    body : 'mauvais mail'
-                  };
+                  return lareponse;
+                });
               }
             })
-          }
+          })
         }
-        
-    
-    )
-  
+        else {
+          return 'mauvais mail';
+        }
+      })
+    }
+
     context.res = {
-      Headers: {"Content-Type": "application/json"},
-      body: 'yeah'
+      body: lareponse
+    }
+
+    
   }
-  }
-      
-      catch(error) {
+    catch(error) {
       const err = error;
       context.res = {
         status: 500,
         body: `Request error. ${err}`
       };
-    } 
-        
+    }
 };
