1ba50daed34f089b5024f59d66bb541566bd7927
L_Beno <benoit@ahgency.be>
2022-10-14 09:27:33 +0200

    Add product link

--- frontend/src/Component/Credits/Credits.js
+++ frontend/src/Component/Credits/Credits.js
@@ -16,11 +16,29 @@ export default function Boutique() {
                <h1 className="resume">Commander des heures {currentNOM}</h1>
 
                
-               <stripe-pricing-table pricing-table-id="prctbl_1LmYYuKlBZDgS2fNGsBqBD0A"
+              {/*  <stripe-pricing-table pricing-table-id="prctbl_1LmYYuKlBZDgS2fNGsBqBD0A"
                publishable-key="pk_test_51JPNMRKlBZDgS2fN3K4QCPMI17QNsbXjNgIRTxSpl6YX01ZvRMGH68W8M2INZUwvdWzIoDGo5uKzmZzN21PSNvYX00jWbaWGea"
                client-reference-id={currentMAIL}
                customer-email={currentMAIL}>
-               </stripe-pricing-table>
+               </stripe-pricing-table> */}
+
+               <div>
+                    Pack 10 heures
+                    <a href='https://buy.stripe.com/28og183lff8T9RS8wx' client-reference-id={currentMAIL}
+               customer-email={currentMAIL}>Acheter 10 heures</a>
+               </div>
+               <div>
+                    Pack 5 heures
+                    <a href='https://buy.stripe.com/aEU7uCdZT1i35BC8wy' client-reference-id={currentMAIL}
+               customer-email={currentMAIL}>Acheter 5 heures</a>
+               </div>
+               <div>
+                    Pack 1 heures
+                    <a href='https://buy.stripe.com/fZe6qybRLd0L4xy5kk' client-reference-id={currentMAIL}
+               customer-email={currentMAIL}>Acheter 1 heures</a>
+               </div>
+               
+               
          </div>
 
      )
