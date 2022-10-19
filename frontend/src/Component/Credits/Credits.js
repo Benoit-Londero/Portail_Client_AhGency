2917ca7b60d599f4304c4f6c3c8d0a49a98856e2
L_Beno <benoit@ahgency.be>
2022-10-19 11:07:58 +0200

    retrait icone page credit.js

--- frontend/src/Component/Credits/Credits.js
+++ frontend/src/Component/Credits/Credits.js
@@ -62,7 +62,7 @@ export default function Boutique() {
                          <h2>Toujours pas convaincu ? Ou vous avez la moindre question ?</h2>
                          <p> N'hésitez pas à nous contacter au 02 319 29 20 ou bien à l'adresse <a href='mailto:info@ahgency.be'>info@ahgency.be</a> </p>
 
-                         <a href="https://www.ahgency.be" class="btn_to_agc" rel="noreferrer" target="_blank"><HiArrowNarrowLeft/>Visitez le site</a>
+                         <a href="https://www.ahgency.be" class="btn_to_agc" rel="noreferrer" target="_blank">Visitez le site</a>
 
                     </div>
                     <img src={illu} alt='illustration'></img>
