fca9ecf05be958769fd0b8cac587127f0f49b9c3
Benoit Londero <benoit@ahgency.be>
2022-11-07 11:02:15 +0100

    Import logo + modification input mail + label "adresse email"

--- frontend/src/Component/Connexion/Connexion.js
+++ frontend/src/Component/Connexion/Connexion.js
@@ -9,6 +9,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Alert from 'react-bootstrap/Alert';
 
+import ahgency_logo from '../img/logo_ahgency';
+
 import {HiArrowNarrowLeft} from 'react-icons/hi';
 
 export default function Connexion(props) {
@@ -34,10 +36,11 @@ export default function Connexion(props) {
             <Row className="connexion_form">
                 <Col lg={12}>
                     <h1>AHGENCY CREATIVE STUDIO</h1>
+                    {ahgency_logo}
                 {erreur === true ? <Alert variant="danger">Email ou mot de passe érroné</Alert> : null }
                 <form id="logForm" onSubmit={handleSubmit}>
-                    <label>Login<br/>
-                        <input type="text" name="email" placeholder="Votre email" required/>
+                    <label>Adresse email<br/>
+                        <input type="mail" name="email" placeholder="Votre email" required/>
                     </label><br/>
                     <label>Mot de passe<br/>
                         <input type="password" name="pwd" placeholder="Votre mot de passe" required/><br></br>
