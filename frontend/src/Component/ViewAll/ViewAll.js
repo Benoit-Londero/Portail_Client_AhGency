b38c5702c595ee89a8a958d3b0f74945fb043866
Benoit Londero <benoit@ahgency.be>
2022-11-08 13:38:15 +0100

    Correction erreur ViewAll.js

--- frontend/src/Component/ViewAll/ViewAll.js
+++ frontend/src/Component/ViewAll/ViewAll.js
@@ -83,7 +83,7 @@ export default function ViewAll() {
                               </tr>
                          </tbody>
                     </table>
-               <{/* /Col>
+          {/* </Col>
           </Row>
           <Row className="customer_card_all timesheet">
                <Col> */}
