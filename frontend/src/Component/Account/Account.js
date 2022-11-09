5778f5e2820297e237958123a83c43612d7a3d51
Benoit Londero <benoit@ahgency.be>
2022-11-09 14:39:44 +0100

    Test installation accordeon

--- frontend/src/Component/Account/Account.js
+++ frontend/src/Component/Account/Account.js
@@ -8,6 +8,7 @@ import NavBar from "../NavBar/NavBar";
 import { RiAccountCircleFill } from "react-icons/ri";
 import { ImMail4 } from "react-icons/im";
 import * as BiIcons from "react-icons/bi";
+import Accordion from 'react-bootstrap/Accordion';
 
 
 export default function NameForm() {
@@ -159,6 +160,37 @@ export default function NameForm() {
 
                          </Col>
                     </Row> */}
+
+                    <Row>
+                         <Col>
+                              <Accordion defaultActiveKey="0">
+                                   <Accordion.Item eventKey="0">
+                                   <Accordion.Header>Accordion Item #1</Accordion.Header>
+                                   <Accordion.Body>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                        culpa qui officia deserunt mollit anim id est laborum.
+                                   </Accordion.Body>
+                                   </Accordion.Item>
+                                   <Accordion.Item eventKey="1">
+                                   <Accordion.Header>Accordion Item #2</Accordion.Header>
+                                   <Accordion.Body>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                                        culpa qui officia deserunt mollit anim id est laborum.
+                                   </Accordion.Body>
+                                   </Accordion.Item>
+                              </Accordion>
+                         </Col>
+                    </Row>
                </Container>        
          </div>
      )
