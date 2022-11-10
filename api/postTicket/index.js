e0ae280f78f8003a4d43f6e576fab0effd4defe8
Benoit Londero <benoit@ahgency.be>
2022-11-10 15:57:02 +0100

    Modification index.js (request ticket)

--- api/postTicket/index.js
+++ api/postTicket/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
+    let name = context.req.body.email;
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
