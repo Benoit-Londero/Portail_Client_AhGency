2b2614f2686c0a9d8cdaf8a0b736a3632138c276
Benoit Londero <benoit@ahgency.be>
2022-11-28 15:38:09 +0100

    Debut design UI page Entreprise

--- frontend/src/Component/Entreprise/Entreprise.js
+++ frontend/src/Component/Entreprise/Entreprise.js
@@ -14,6 +14,9 @@ export default function Entreprise() {
   const [currentSITE, setCurrentSITE] = useState();
   const [currentMAINTENANCE, setCurrentMAINTENANCE] = useState();
 
+
+  const users = ['fabian','Benoit','Quentin'];
+
   useEffect(() => {
     let dataU = {currentIDEntreprise: currentIDE};
           
@@ -44,17 +47,51 @@ export default function Entreprise() {
   }, [currentIDE])
 
   return (
-    <div>
+    <div id="page_entreprise">
           <NavBar/>
-          <h1>Entreprise</h1>
+          <Container>
+               <h1>Entreprise</h1>
+               <Row>
+                    <form id="editForm" onSubmit={handleClick}>
+                         <table className="Profil">
+                              <tbody>
+                                   <tr><td><RiAccountCircleFill className="account_ppic"/></td></tr>
+                                   <tr>
+                                        <td><label className="bold">Nom : </label> <input type="text" name="nom" placeholder="Nom de l'entreprise" defaultValue ={currentNomE} required/></td>
+                                        <td><label className="bold">Numéro de TVA: </label> <input type="text" name="tva" placeholder="BE123456789" defaultValue ={currentTVA} required/></td>
+                                   </tr>
+                                   <tr><td colspan="2"><label className="bold"> Adresse : </label><input type="text" name="adresse" placeholder="Votre adresse" defaultValue ={currentADRESSE} required/></td></tr>
+                                   <tr><td colspan="2"><label className="bold"> Téléphone : </label><input type="tel" id="telephone" name="telephone" placeholder="01/234.567" defaultValue ={currentTEL}></input></td></tr>
 
-          Nom : {currentNomE}
-          TVA : {currentTVA}
-          Adresse : {currentADRESSE}
-          Téléphone : {currentTEL}
-          Email : {currentEMAILE}
-          Site : {currentSITE}
-          Maintenance : { currentMAINTENANCE === 1 ? "Contrat de maintenance OK" : "Contrat de maintenance NOK"}
+                                   <tr><td colspab="2"><label className="bold"> Email : </label><input type="mail" id="email" name="email" placeholder="bernard@bouchard.be" defaultValue= {currentEMAILE}></input></td></tr>
+                                   <tr><td colspab="2"><label className="bold"> Site : </label><input type="text" id="web" name="web"  defaultValue= {currentSITE}></input></td></tr>
+                                   <tr><td colspab="2"><label className="bold"> Email : </label><input type="mail" id="email" name="email" placeholder="bernard@bouchard.be" defaultValue= {currentEMAILE}></input></td></tr>
+                                   
+                                   <tr><td><input type="hidden" name="idu" value ={currentIDU}/></td></tr>
+                                   <tr><td colspan="3"><input type="submit" name="modifier" value="Enregistrer" /></td></tr>
+                              </tbody>
+                         </table>
+                    </form>
+
+                    <p>Maintenance : { currentMAINTENANCE === 1 ? "Contrat de maintenance OK" : "Contrat de maintenance NOK"}</p>
+
+                    <form>
+                         <label>Ajouter des membres à mon entreprise </label>
+                         <select>
+                              {
+                              users.map((item,index) => {
+                                   return(
+                                        <option key={index} value={item}>{item}</option>
+                                   )
+                              })
+                              }
+                         </select>
+                    </form>
+
+               </Row>
+
+          
+          </Container>
      </div>
   )
 }
