50cdbf4841c21d8e65098ea7a8c9f1ad629f56e5
Benoit Londero <benoit@ahgency.be>
2022-11-29 10:14:09 +0100

    Use filter & map pour contact Administrateur + API getUsers

--- frontend/src/Component/Account/Account.js
+++ frontend/src/Component/Account/Account.js
@@ -30,7 +30,8 @@ export default function NameForm() {
      const [displayEntreprise, seCurrentEntreprise] = useState();
 
      const currentIDE = (localStorage.getItem("currentIDE").replaceAll('"',''));
-     const users = ['fabian','Benoit','Quentin'];
+
+     const [users, setAllUsers] = useState();
 
      useEffect(() => {
           let dataU = {currentIDUser: currentIDU};
@@ -73,27 +74,17 @@ export default function NameForm() {
                     } else {
                          
                     }
-               }
+               
+               fetch('/api/getUsers')
+                    .then(resp => resp.json())
+                    .then(json => setAllUsers(json))
+                    .catch(err => console.info(err))
+          }
 
           onLoad();
 
      }, [currentIDU, currentIDE])
       
-
-     const contact_agc = [
-          {
-               nom : 'Quentin de Jarnac',
-               mail : 'quentin@ahgency.be'
-          },
-          {
-               nom : 'Fabian Hernandez',
-               mail : 'fabian@ahgency.be'
-          },
-          {
-               nom : 'Benoit Londero',
-               mail : 'benoit@ahgency.be'
-          }
-     ]
 
      const handleClick = async e => {
           e.preventDefault();
@@ -175,14 +166,14 @@ export default function NameForm() {
                          <Col md={{span: 3, offset: 1}} className="my_contact">
                          <h2>Envoyer un mail à</h2>
                               <ul>
-                                   {contact_agc.map((item,index) => {
+                                   {users.filter(item => item.Role === 'administrator').map((item,index) => {
                                         return (
                                              <li key={index}>
                                                   <table>
                                                        <tbody>
                                                             <tr>
-                                                            <td>{item.nom}<br/><span  className="mail">{item.mail }</span></td>
-                                                            <td><a href={"mailto:" + item.mail }><ImMail4 className="btn_mail"/></a></td>
+                                                            <td>{item.Nom}<br/><span  className="mail">{item.Email }</span></td>
+                                                            <td><a href={"mailto:" + item.Email }><ImMail4 className="btn_mail"/></a></td>
                                                             </tr>
                                                        </tbody>
                                                   </table>
