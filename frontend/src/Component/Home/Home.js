9de6de6a820a67a150a9b0c18bbc4f6da2f15e81
Benoit Londero <benoit@ahgency.be>
2022-12-12 15:50:04 +0100

    size colonne temps alloué

--- frontend/src/Component/Home/Home.js
+++ frontend/src/Component/Home/Home.js
@@ -199,7 +199,7 @@ export default function Home() {
                                    <tr>
                                         <td key={index} rowspan="3"><p className="date_badge">{item.Tickets.substring(0,1)}</p></td>
                                         <td><h1>{item.Tickets}</h1><p className="date_creation">{Moment(item.Date).format('DD-MM-YYYY')}</p></td>
-                                        <td><p className="ref allowed_time">Temps alloué : {Math.trunc(item.AllocationTemps /60)} h {item.AllocationTemps % 60 } min</p></td>
+                                        <td className="col__timeToUse"><p className="ref allowed_time">Temps alloué : {Math.trunc(item.AllocationTemps /60)} h {item.AllocationTemps % 60 } min</p></td>
                                    </tr>)
                               })
                               }
