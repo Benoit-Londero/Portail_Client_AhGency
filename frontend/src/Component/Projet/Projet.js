c62fb81178dbc7764b5d0531d002c1f3b9c20b4e
Benoit Londero <benoit@ahgency.be>
2022-12-23 15:36:23 +0100

    ajotu break case heures

--- frontend/src/Component/Projet/Projet.js
+++ frontend/src/Component/Projet/Projet.js
@@ -88,6 +88,7 @@ export default function Projet() {
                 break;
             case 'heures':
                 setFormDisplay('heures');
+                break;
 
             default :
                 setFormDisplay('projet');
