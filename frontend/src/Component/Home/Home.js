55d6bd77f0a7335702bccb430e9a6995364334c5
Benoit Londero <benoit@ahgency.be>
2022-12-23 15:02:14 +0100

    Ajout statistique homepage

--- frontend/src/Component/Home/Home.js
+++ frontend/src/Component/Home/Home.js
@@ -28,6 +28,11 @@ export default function Home() {
      const [newTask, setnewTask] = useState(false);
      const [detailTask, setdetailTask] = useState(false);
 
+     const [currentHeureTOT, setCurrentHeureTOT] = useState();
+     const [currentHeureREST, setCurrentHeureREST] = useState();
+     const [moneySpend, setMoneySpend] = useState();
+     const [checkPercent, setCheckPercent] = useState();
+
      const currentIDU = localStorage.getItem("currentIDU");
      const currentIDE = localStorage.getItem("currentIDE");
 
@@ -51,6 +56,40 @@ export default function Home() {
           .then(res => res.json())
           .then(json => setProjet(json))
           .catch(err => console.info(err))
+
+          const onLoad = async () => {
+               
+               const response = await fetch('/api/getInfosClient', { 
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify(dataU)
+               })
+               
+               const data = await response.json();
+          
+               if(response.status === 200){
+                    setCurrentHeureTOT(data.Minutes_Achetees);
+                    setCurrentHeureREST(data.Minutes_Restantes);
+
+                    //Calcul temps restants (On soustrait le temps dépensé au temps total)
+                    const timeSpend = data.Minutes_Achetees - data.Minutes_Restantes;
+
+                    //Calcul du montant dépensé (temps dépensé)
+                    setMoneySpend(Math.round(((timeSpend/60) * 75)));
+
+                    if (parseInt(data.Minutes_Achetees) === 0) {
+                         const percentage = 0;
+                         setCheckPercent(percentage);
+                    } else {
+                         const percentage = Math.round(((100*data.Minutes_Restantes) / data.Minutes_Achetees));
+                         setCheckPercent(percentage);
+                    }
+               } else {
+                    alert('Erreur du serveur, veuillez réessayer plus tard');
+               }
+          }
+
+          onLoad();
 
      }, [currentIDU, currentIDE])
 
@@ -103,6 +142,62 @@ export default function Home() {
      </div>
 
      <Container id="page_dashboard"  className="main__content">
+          <Row>
+               <div className="stats">
+                    <h2>Statistiques</h2>
+                                        
+                    {checkPercent > 10 ? <CircularProgressbar
+                         value={checkPercent}
+                         text={`${checkPercent}%`}
+                         styles={{
+                              path: {
+                                   strokeLinecap: 'round',
+                                   transition: 'stroke-dashoffset 0.5s ease 0s',
+                                   stroke: '#3FB58F'
+                              },
+                              // Customize the circle behind the path, i.e. the "total progress"
+                              trail: {
+                                   stroke: '#e7e7e7',
+                                   strokeLinecap: 'round',
+                              },
+                                   
+                              text: {
+                                   transform: 'translate(-20px, 5px)',
+                                   fontSize: '15px',
+                                   fill: '#fff'
+                              }
+                    
+                         }}
+                    /> : <CircularProgressbar
+                         value={checkPercent}
+                         text={`${checkPercent}%`}
+                         styles={{
+                         path: {
+                              strokeLinecap: 'round',
+                              transition: 'stroke-dashoffset 0.5s ease 0s',
+                              stroke: '#FF0000'
+                         },
+                         
+                         // Customize the circle behind the path, i.e. the "total progress"
+                         trail: {
+                              stroke: '#e7e7e7',
+                              strokeLinecap: 'round',
+                         },
+                              
+                         text: {
+                              transform: 'translate(-20px, 5px)',
+                              fontSize: '15px',
+                              fill: '#FF0000'
+                         }
+                    }}
+                    />}
+                         
+                    <p>Achetées : {Math.round(currentHeureTOT /60)} h</p>
+                    <p>Restantes : {Math.trunc(currentHeureREST /60)} h {currentHeureREST % 60 } min</p><br/>
+                    <p><b>Dépensé : {moneySpend} €</b></p>
+                    {checkPercent > 10 ? null : <Link to ='/Credits'><Button className="recharger">Recharger</Button></Link>}
+               </div>
+          </Row>
           <Row>
                <Col className="tableauTS">
                     <table>
