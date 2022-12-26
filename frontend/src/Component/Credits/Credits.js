915407ef31e050f138299f26c909559dfa9d6770
Benoit Londero <benoit@ahgency.be>
2022-12-26 13:34:44 +0100

    Modification page credit + css home page

--- frontend/src/Component/Credits/Credits.js
+++ frontend/src/Component/Credits/Credits.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import NavBar from '../NavBar/NavBar.js';
 import './Credits.css';
 import illu from '../../img/Brainstorming_session_Monochromatic.png';
+import { FaLightbulb } from 'react-icons/fa';
 
 export default function Boutique() {
 
@@ -14,32 +15,34 @@ export default function Boutique() {
                <div id="page_boutique"  className="main__content">
                     <h1>Commander des heures</h1>
 
-                    <div  class="product_fiche">
-                         <h2>Pack 1 heures</h2>
-                         <p class="price">75 €</p>
+                    <ul>
+                         <li class="product_fiche">
+                              <h2>Pack 1 heures</h2>
+                              <p class="price">75 €</p>
 
-                         <p class="descp">Un pack d'une heure utile pour vos petites modifications tel que des changements de couleur, de texte ou d'image.</p>
+                              <p class="descp">Un pack d'une heure utile pour vos petites modifications tel que des changements de couleur, de texte ou d'image.</p>
 
-                         <a href='https://buy.stripe.com/fZe6qybRLd0L4xy5kk' rel="noopener noreferrer" target="_blank" class="btn primary_btn">Acheter 1 heures</a>                    
-                    </div>
+                              <a href='https://buy.stripe.com/fZe6qybRLd0L4xy5kk' rel="noopener noreferrer" target="_blank" class="btn primary_btn">Acheter 1 heures</a>         
+                         </li>
 
-                    <div class="product_fiche pdt_highlight">
-                         <h2>Pack 5 heures</h2>
-                         <p class='price'>325 €</p>
+                         <li class="product_fiche">
+                              <h2>Pack 5 heures</h2>
+                              <p class='price'>325 €</p>
 
-                         <p class="descp">Vous avez pensé à une nouvelle fonctionnalité pour votre site, ou un besoin d'ajout de contenu, ce pack est fait pour vous.</p>
+                              <p class="descp">Vous avez pensé à une nouvelle fonctionnalité pour votre site, ou un besoin d'ajout de contenu, ce pack est fait pour vous.</p>
 
-                         <a href='https://buy.stripe.com/aEU7uCdZT1i35BC8wy' rel="noopener noreferrer" target="_blank" class="btn primary_btn">Acheter 5 heures</a>
-                    </div>
-               
-                    <div class="product_fiche">
-                         <h2>Pack 10 heures</h2>
-                         <p class="price">600 €</p>
+                              <a href='https://buy.stripe.com/aEU7uCdZT1i35BC8wy' rel="noopener noreferrer" target="_blank" class="btn primary_btn">Acheter 5 heures</a>
+                         </li>
 
-                         <p class="descp">Envie de faire peau neuve, de procéder à une refonte de votre site ou de tout simplement prévoir un "capital" temps pour vos futures demandes, ce pack répondra à vos attentes.</p>
+                         <li class="product_fiche">
+                              <h2>Pack 10 heures</h2>
+                              <p class="price">600 €</p>
 
-                         <a href='https://buy.stripe.com/28og183lff8T9RS8wx' rel="noopener noreferrer" target="_blank" class="btn primary_btn">Acheter 10 heures</a>
-                    </div>
+                              <p class="descp">Envie de faire peau neuve, de procéder à une refonte de votre site ou de tout simplement prévoir un "capital" temps pour vos futures demandes, ce pack répondra à vos attentes.</p>
+
+                              <a href='https://buy.stripe.com/28og183lff8T9RS8wx' rel="noopener noreferrer" target="_blank" class="btn primary_btn">Acheter 10 heures</a>
+                         </li>
+                    </ul>
 
                     <div id="catchow">
                          <div class="col-gch">
