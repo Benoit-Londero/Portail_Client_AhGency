4b4e8ffec9fb7722954df569f50bcaa999825a85
Benoit Londero <benoit@ahgency.be>
2022-12-29 19:30:00 +0100

    repush

--- api/getInfosEntreprise/index.js
+++ api/getInfosEntreprise/index.js
@@ -9,8 +9,8 @@ module.exports = async function (context, req) {
     informations = await new Promise((resolve,reject) => {
 
         let IDE = req.body.currentIDEntreprise;
-
-/*         let query = "SELECT e.ID_entreprise, e.Nom_societe, e.TVA, e.Adresse, e.Telephone, e.Email, e.Date_creation, e.Maintenance, e.Site_web, e.Membres, u.ID, u.Nom, u.Prenom FROM entreprise as e JOIN users as u on e.ID_entreprise = u.ID_entreprise WHERE e.ID_entreprise = ?"; */
+        
+        /*let query = "SELECT e.ID_entreprise, e.Nom_societe, e.TVA, e.Adresse, e.Telephone, e.Email, e.Date_creation, e.Maintenance, e.Site_web, e.Membres, u.ID, u.Nom, u.Prenom FROM entreprise as e JOIN users as u on e.ID_entreprise = u.ID_entreprise WHERE e.ID_entreprise = ?"; */
         let query = "SELECT * FROM entreprise WHERE ID_entreprise = ?"
         req = con.query(query, [IDE], function (err,rows){
             if (err) throw err;
