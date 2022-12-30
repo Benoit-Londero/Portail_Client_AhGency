93e3f954315fa1c88279776c80b555eab5517195
Benoit Londero <benoit@ahgency.be>
2022-12-30 10:19:34 +0100

    modification ternaire

--- frontend/src/Component/Clients/Clients.js
+++ frontend/src/Component/Clients/Clients.js
@@ -267,12 +267,13 @@ export default function Clients() {
                <li><Button className='btn noborder' value="clients" onClick={handleEntreprise}>Clients</Button></li>
                <li><Button className='btn noborder' value="entreprise" onClick={handleEntreprise}>Entreprise</Button></li>
                <li></li>
-               <li>{showDetailsEntr === false ? <label className="bold">Ajouter un client
+               <li>
+                    {showEntreprise === false ? <label className="bold">Ajouter un client
                     <Button className="btn primary_btn" onClick={handleAddClient}>+</Button></label>
-               :
-               <label>Ajouter une entreprise<br></br>
-               <Button className="btn primary_btn" onClick={handleAddClient}>+</Button>
-               </label>}
+                    :
+                    <label>Ajouter une entreprise<br></br>
+                    <Button className="btn primary_btn" onClick={handleAddClient}>+</Button>
+                    </label>}
                </li>
           </ul>
      </div>
