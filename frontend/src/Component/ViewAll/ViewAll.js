fb8852250d67e44089948c9870fa0f4c90f887f9
Benoit Londero <benoit@ahgency.be>
2023-01-12 10:16:01 +0100

    input submit value

--- frontend/src/Component/ViewAll/ViewAll.js
+++ frontend/src/Component/ViewAll/ViewAll.js
@@ -84,7 +84,6 @@ export default function ViewAll() {
           setFiltredtasks(ProjetFiltertask(theProjet));
      }
 
-
      const closeTasks = (e) => {
           setdetailTask(false);
           setupdateTask(false);
@@ -297,7 +296,7 @@ export default function ViewAll() {
                          {alltasks.filter(item => item.ID_TS === parseInt(value_dtls)).map((item,index) => {
 
                          return(
-                         <form id="updateTask" onSubmit={handleUpdate} method="POST">
+                         <form id="updateTask" onSubmit={handleUpdate}>
                               <table className="detail_TS" key={index}>
                                    <thead>
                                         <tr>
@@ -323,8 +322,8 @@ export default function ViewAll() {
                                              <td><input type="number" name="timeSpend"></input></td>
                                         </tr>
                                         <tr><td><p className="bold"><BsIcons.BsTextParagraph/>Description</p></td></tr>
-                                        <tr><td colspan="2"><textarea name="descr_" className="tasks">{ item.Informations}</textarea></td></tr>
-                                        <tr><td><input type="submit" name="updateTask" >Mettre à jour</input>
+                                        <tr><td colspan="2"><textarea name="descr_" className="tasks">{item.Informations}</textarea></td></tr>
+                                        <tr><td><input type="submit" name="updateTask" value="Mettre à jour"></input>
                                         <input type="hidden" name="id_task" value={item.ID}></input></td></tr>
                                    </tbody>
                               </table>
