4c5c84930f85075c5f116b0bf16692d1c01a2a7d
Benoit Londero <benoit@ahgency.be>
2023-01-19 15:49:52 +0100

    try fix issue found by chatGPT

--- frontend/src/Component/ChatBox/ChatBox.js
+++ frontend/src/Component/ChatBox/ChatBox.js
@@ -1,4 +1,4 @@
-/* import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import MessageList from '../MessageList/MessageList';
 import Container from "react-bootstrap/esm/Container";
 import NavBar from "../NavBar/NavBar";
@@ -11,40 +11,43 @@ export default function ChatBox(){
      const [oldMessage, setOldermessage] = useState([]);
      const [AllProjet, setAllProjects] = useState([]);
      const [idEntreprise, setCurrentIDE] = useState();
+     const [timeoutId, setTimeoutId] = useState();
+     const formRef = useRef(null);
+
+     console.log(timeoutId);
 
      const currentIDU = localStorage.getItem("currentIDU");
 
-     useEffect (() => {
-          let dataU = {currentIDE: idEntreprise};
-          console.log(dataU)
-          async function fetchMessages() {
-               try {
-                 const response = await fetch('/api/getMessages', {
-                    method: 'POST',
-                    body : JSON.stringify(dataU)
-                 });
-                 const data = await response.json();
-                 setOldermessage(data);
-               } catch (error) {
-                 console.error('Error fetching messages:', error);
-               }
+     async function fetchMessages() {
+          try {
+            const response = await fetch('/api/getMessages', {
+               method: 'POST',
+               body : JSON.stringify({currentIDE: idEntreprise, currentIDU})
+            });
+            const data = await response.json();
+            setOldermessage(prevMessages => [...prevMessages, ...data]);
+          } catch (error) {
+            console.error('Error fetching messages:', error);
           }
+     }
+
+     useEffect (() => {
 
           fetch('/api/getAllProjet')
           .then(res => res.json())
           .then(json => setAllProjects(json))
           .catch(err => console.info(err))
 
-          const intervalId = setInterval(fetchMessages, 5000);
-          return () => clearInterval(intervalId);
+          const timeoutId = setTimeout(fetchMessages, 5000);
+          setTimeoutId(timeoutId);
+          return () => clearTimeout(timeoutId);
           
      }, [])
 
      const sendMessage = async (e) => {
           e.preventDefault();
 
-          let newMess = document.getElementById('submitMessage');
-          let postMess = new FormData(newMess);
+          let postMess = new FormData(formRef.current);
 
           const jsonForm = buildJsonFormData(postMess);
 
@@ -53,7 +56,6 @@ export default function ChatBox(){
             for(const pair of postMess){
                 jsonFormData[pair[0]] = pair[1];
             }
-
             return jsonFormData; // On retourne l'objet pour pouvoir l'envoyer
           }
 
@@ -65,6 +67,7 @@ export default function ChatBox(){
                });
                const data = await response.json();
                console.log('Message sent successfully:', data);
+               fetchMessages();
              } catch (error) {
                console.error('Error sending message:', error);
           }
@@ -74,6 +77,7 @@ export default function ChatBox(){
           const pid = e.target.value;
           console.log(pid);
           setCurrentIDE(pid);
+          fetchMessages()
      }
 
   return (
@@ -107,96 +111,4 @@ export default function ChatBox(){
                </Container>
           </div>
   );
-} */
-
-import React, { useState, useEffect, useRef } from 'react';
-import MessageList from '../MessageList/MessageList';
-import Container from "react-bootstrap/Container";
-import NavBar from "../NavBar/NavBar";
-import Button from 'react-bootstrap/Button';
-import './ChatBox.css'
-
-export default function ChatBox() {
-    const [oldMessage, setOldermessage] = useState([]);
-    const [AllProjet, setAllProjects] = useState([]);
-    const [idEntreprise, setCurrentIDE] = useState();
-    const [timeoutId, setTimeoutId] = useState();
-    const formRef = useRef(null);
-
-
-    console.log(timeoutId);
-    const currentIDU = localStorage.getItem("currentIDU");
-    async function fetchMessages() {
-        try {
-            const response = await fetch('/api/getMessages', {
-                method: 'POST',
-                body: JSON.stringify({ currentIDE: idEntreprise, currentIDU }),
-                headers: { 'Content-Type': 'application/json' },
-            });
-            const data = await response.json();
-            setOldermessage(prevMessages => [...prevMessages, ...data]);
-        } catch (error) {
-            console.error('Error fetching messages:', error);
-        }
-    }
-    useEffect(() => {
-        Promise.all([
-            fetch('/api/getAllProjet')
-                .then(res => res.json())
-                .then(json => setAllProjects(json))
-                .catch(err => console.info(err)),
-            fetchMessages()
-        ])
-        const timeoutId = setTimeout(fetchMessages, 5000);
-        setTimeoutId(timeoutId);
-        return () => clearTimeout(timeoutId);
-    }, [idEntreprise]);
-
-    const sendMessage = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await fetch('/api/postMessage', {
-                method: 'POST',
-                body: new FormData(formRef.current),
-                headers: { 'Content-Type': 'application/json' },
-            });
-            const data = await response.json();
-            console.log('Message sent successfully:', data);
-            fetchMessages();
-        } catch (error) {
-            console.error('Error sending message:', error);
-        }
-    }
-
-    const handleChange = (e) => {
-        const pid = e.target.value;
-        console.log(pid);
-        setCurrentIDE(pid);
-    }
-
-    return (
-     <div>
-         <NavBar />
-         <div className="project_sidebar">
-             {AllProjet.map((item, index) => {
-                 return (
-                     <li key={index}><Button className="btn_primary" onClick={handleChange} value={item.ID}>{item.Tickets}</Button></li>
-                 )
-             })}
-         </div>
-         <Container id="page_dashboard" className="main__content">
-             <MessageList messages={oldMessage} />
-             <form ref={formRef} onSubmit={sendMessage}>
-                 <input
-                     type="text"
-                     placeholder="Enter your message"
-                     name="mess"
-                 />
-                 <input type="hidden" name="currentIDE" value={idEntreprise}></input>
-                 <input type="hidden" name="currentIDU" value={currentIDU}></input>
-                 <button type="submit">envoyer mon message</button>
-             </form>
-         </Container>
-     </div>
- )
 }
