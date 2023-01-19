324f1eefa26a02cb7e65d2de5281ae7799b640cb
Benoit Londero <benoit@ahgency.be>
2023-01-19 15:58:15 +0100

    remove if/else messageList

--- frontend/src/Component/MessageList/MessageList.js
+++ frontend/src/Component/MessageList/MessageList.js
@@ -1,30 +1,13 @@
 import React from 'react';
 
-const render = (message) => {
-  const currentIDU = localStorage.getItem("currentIDU");
-  
-  if(message.ID_client === currentIDU){
-    return(
-      <li className='sendByMe' key={message.id}>
-        <p>{message.ID_client} à envoyé</p>
-        <p>{message.Message}</p>
-      </li>
-    )
-  } else {
-    return(
-      <li className='sendToMe' key={message.id}>
-        <p>{message.ID_client} à envoyé</p>
-        <p>{message.Message}</p>
-      </li>
-    )
-  }
-}
-
 function MessageList({ messages }) {
   return (
     <ul>
       {messages.map(message => (
-        {render}
+        <li className='sendBy' key={message.id}>
+          <p>{message.ID_client} à envoyé</p>
+          <p>{message.Message}</p>
+      </li>
       ))}
     </ul>
   );
