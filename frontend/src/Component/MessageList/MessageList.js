13cd3bcaabe6ca4940557baaf0b3bc3c8634045d
Benoit Londero <benoit@ahgency.be>
2023-01-23 09:03:49 +0100

    Modification affichage message  ( nom ≠ content message + ajout nom users)

--- frontend/src/Component/MessageList/MessageList.js
+++ frontend/src/Component/MessageList/MessageList.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+const [nameUser, setnameUser] = useState();
+
+useEffect(() => {
+  fetch('/api/getAllUsers')
+  .then(response => response.json())
+  .then(json => setnameUser(json))
+  .catch(error => console.info(err))
+
+}, [])
 
 function MessageList({ messages, className }) {
   return (
     <ul>
       {messages.map(message => (
         <li className={className(message)} key={message.id}>
-          <p>{message.ID_client} à envoyé</p>
-          <p>{message.Message}</p>
+          <p>{nameUser.filter(donnee => donnee.ID === message.ID_client).map((item,index) => {
+            return(<span className='bdg_user'>{item.Prenom.substring(0,1)}</span> + ' ' + item.Prenom)
+          })} à envoyé</p>
+          <p className='contentMessage'>{message.Message}</p>
       </li>
       ))}
     </ul>
