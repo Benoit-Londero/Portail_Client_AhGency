79d2dc5b51b202d29c8115da04a45b4c994ed853
Benoit Londero <benoit@ahgency.be>
2023-01-23 09:29:03 +0100

    retrait function affichage nom sender / receiver

--- frontend/src/Component/MessageList/MessageList.js
+++ frontend/src/Component/MessageList/MessageList.js
@@ -1,23 +1,11 @@
-import React, {useEffect, useState} from 'react';
-
-const [nameUser, setnameUser] = useState();
-
-useEffect(() => {
-  fetch('/api/getAllUsers')
-  .then(response => response.json())
-  .then(json => setnameUser(json))
-  .catch(err => console.info(err))
-
-}, [])
+import React from 'react';
 
 function MessageList({ messages, className }) {
   return (
     <ul>
       {messages.map(message => (
         <li className={className(message)} key={message.id}>
-          <p>{nameUser.filter(donnee => donnee.ID === message.ID_client).map((item,index) => {
-            return(<span className='bdg_user'>{item.Prenom.substring(0,1)}</span> + ' ' + item.Prenom)
-          })} à envoyé</p>
+          <p>{message.ID_client} à envoyé</p>
           <p className='contentMessage'>{message.Message}</p>
       </li>
       ))}
