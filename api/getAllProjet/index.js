b4b0683c1a682694afc96ff271ae1bfdeed4ecb0
Benoit Londero <benoit@ahgency.be>
2023-02-02 16:36:30 +0100

    ajout allow cors policy in getAllPrjet API

--- api/getAllProjet/index.js
+++ api/getAllProjet/index.js
@@ -23,7 +23,7 @@ module.exports = async function (context, req) {
 
   context.res = {
     status: 200,
-    headers: {'Content-Type':'application/json'},
+    headers: {'Content-Type':'application/json', 'Access-Control-Allow-Origin':'*'},
     body: JSON.stringify(projetAll)
   }
 }
